3615c14b1001a359a7875df9c7377928a592d052
GuyGuy5000 <nadavhilu12@gmail.com>
2025-01-02 15:30:15 -0500

    Modified timer callback to use addListenerEvent.
    Added snooze and dismiss buttons

--- scripts/timerLib.js
+++ scripts/timerLib.js
@@ -1,19 +1,21 @@
 //timer class
+//id is used to create a unique id for html elements. Created with a trimmed title + timestamp;
 //intervalID is used to capture the result from setInterval();
 //hours, minutes, and seconds are mutable and change when the timer is ticking
-//title and message are used to differentiate each timer
+//title is used to differentiate each timer
 //isPaused is a flag for whether the timer is paused or not
 //time array contains the original values passed into the timer to be able to reset hours, minutes, and seconds
 class timer {
-  constructor(hours, minutes, seconds, title, message) {
+  constructor(hours, minutes, seconds, title) {
+    this.id = title.replace(/\s/g, "") + Date.now();
     this.intervalID;
     this.hours = hours;
     this.minutes = minutes;
     this.seconds = seconds;
     this.title = title;
-    this.message = message;
     this.isPaused = true;
     this.time = [hours, minutes, seconds];
+    this.callback = null;
   }
 
   StartTimer(div) {
@@ -22,6 +24,7 @@ class timer {
     //start 1 second intervals
     this.intervalID = setInterval(tick, 1000);
     this.isPaused = false;
+    div.innerHTML = this.toString();
   }
 
   StopTimer() {
@@ -51,10 +54,11 @@ class timer {
     this.seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
     //if difference is less than 0, time is up
-    if (diff < 0) {
+    if (diff <= 0) {
       clearInterval(this.intervalID);
-      alert(this.message);
-      this.ResetTimer(div);
+      if (this.callback != null) this.callback();
+      else alert(`${this.title} is up!`);
+
       return;
     }
 
@@ -76,6 +80,12 @@ class timer {
       -2
     )}:${("0" + this.seconds).slice(-2)}`;
   }
+
+  addEventListener(eventType, callback) {
+    if (eventType !== "done") return;
+    if (typeof callback !== "function") return;
+    this.callback = callback;
+  }
 }
 
 //timerUI class
@@ -90,23 +100,26 @@ class timerUI {
   constructor(timer, containerDiv) {
     this.timer = timer;
     this.containerDiv = containerDiv;
-
     //create HTML
-    this.titleContainer = `<h1 id="${timer.title}title">${timer.title}</h1>`;
-    this.timeContainer = `<h2 id="${timer.title}time">${timer.toString()}</h2>`;
-    this.pauseButton = `<button id="${timer.title}pause">Start</button>`;
-    this.resetButton = `<button id="${timer.title}reset">Reset</button>`;
-    this.buttonContainer = `<div id="${timer.title}btn">${this.pauseButton} ${this.resetButton}</div>`;
-
+    this.titleContainer = `<h1 id="${timer.id}title">${timer.title}</h1>`;
+    this.timeContainer = `<h2 id="${timer.id}time">${timer.toString()}</h2>`;
+    this.pauseButton = `<button id="${timer.id}pause">Start</button>`;
+    this.resetButton = `<button id="${timer.id}reset">Reset</button>`;
+    this.snoozeButton = `<button id="${timer.id}snooze" hidden>Snooze</button>`;
+    this.dismissButton = `<button id="${timer.id}dismiss" hidden>Dismiss</button>`;
+    this.buttonContainer = `<div id="${timer.id}btn">${this.pauseButton} ${this.resetButton} ${this.snoozeButton} ${this.dismissButton}</div>`;
+    //set html
     this.containerDiv.innerHTML = `${this.titleContainer}
                                     ${this.timeContainer} 
                                     ${this.buttonContainer}`;
     //retrieve HTML elements
-    this.titleContainer = document.getElementById(`${timer.title}title`);
-    this.timeContainer = document.getElementById(`${timer.title}time`);
-    this.pauseButton = document.getElementById(`${timer.title}pause`);
-    this.resetButton = document.getElementById(`${timer.title}reset`);
-    this.buttonContainer = document.getElementById(`${timer.title}btn`);
+    this.titleContainer = document.getElementById(`${timer.id}title`);
+    this.timeContainer = document.getElementById(`${timer.id}time`);
+    this.pauseButton = document.getElementById(`${timer.id}pause`);
+    this.resetButton = document.getElementById(`${timer.id}reset`);
+    this.snoozeButton = document.getElementById(`${timer.id}snooze`);
+    this.dismissButton = document.getElementById(`${timer.id}dismiss`);
+    this.buttonContainer = document.getElementById(`${timer.id}btn`);
 
     //set event listeners
     this.pauseButton.addEventListener("click", () => {
@@ -121,23 +134,54 @@ class timerUI {
 
     this.resetButton.addEventListener("click", () => {
       this.timer.ResetTimer(this.timeContainer);
+      this.pauseButton.innerHTML = "Start";
     });
+
+    this.snoozeButton.addEventListener("click", () => {
+      this.timer.hours = 0;
+      this.timer.minutes = 5;
+      this.timer.seconds = 0;
+      this.timer.StartTimer(this.timeContainer);
+      this.resetButton.hidden = false;
+      this.pauseButton.hidden = false;
+      this.snoozeButton.hidden = true;
+      this.dismissButton.hidden = true;
+      this.pauseButton.innerHTML = "Pause";
+    });
+
+    this.dismissButton.addEventListener("click", () => {
+      this.timer.ResetTimer(this.timeContainer);
+      this.resetButton.hidden = false;
+      this.pauseButton.hidden = false;
+      this.snoozeButton.hidden = true;
+      this.dismissButton.hidden = true;
+    });
+  }
+
+  OnDone() {
+    this.pauseButton.innerHTML = "Start";
+    this.resetButton.hidden = true;
+    this.pauseButton.hidden = true;
+    this.snoozeButton.hidden = false;
+    this.dismissButton.hidden = false;
   }
 }
 
-
 //test code
 let timerDiv = document.getElementById("timer");
-let timer2Div = document.getElementById("timer2");
 
-let t = new timer(0, 0, 2, "timer 1", "done!");
+let t = new timer(0, 0, 2, "timer 1");
+t.addEventListener("done", () => {
+  tUI.OnDone();
+});
 let tUI = new timerUI(t, timerDiv);
 
-let t2 = new timer(0, 1, 2, "timer 2", "2 done!");
-let tUI2 = new timerUI(t2, timer2Div);
+// let timer2Div = document.getElementById("timer2");
+// let t2 = new timer(0, 1, 2, "timer 2");
+// let tUI2 = new timerUI(t2, timer2Div);
 
-tUI2.pauseButton.classList.add("test");
-tUI2.resetButton.classList.add("test");
+// tUI2.pauseButton.classList.add("test");
+// tUI2.resetButton.classList.add("test");
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
